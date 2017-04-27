cdb20a566f361b18ff16dfbc1b820aa015c2edd7
Wallace Coelho <wac.almeida@gmail.com>
2017-04-27 11:40:04 -0300

    Base Project configured

---
+++ test/unit/specs/Home.spec.js
@@ -0,0 +1,15 @@
+import Vue from 'vue'
+import router from '@/router'
+
+describe('Home.vue', () => {
+  it('Deve Renderizar e trazer o componente correto', () => {
+    const vm = new Vue({
+      el: document.createElement('div'),
+      router: router,
+      render: h => h('router-view')
+    })
+
+    expect(vm.$el.querySelector('h1').textContent)
+      .to.equal('Pokontest')
+  })
+})
