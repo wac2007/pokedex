f714589a2c6d6d28960c3b99345fbb18161db116
Wallace Coelho <wac.almeida@gmail.com>
2017-04-28 19:49:19 -0300

    Test Search Pokémons

--- test/unit/specs/services/PokemonListService.spec.js
+++ test/unit/specs/services/PokemonListService.spec.js
@@ -5,10 +5,11 @@ import { mockData, removeMock } from '../../helpers'
 
 describe('PokemonListService', () => {
   let serverResponse
+  let service
   before(done => {
     localStorage.clear()
     mockData(data)
-    let service = new PokemonService(Vue.resource)
+    service = new PokemonService(Vue.resource)
     service.listPokemons(0)
       .then(response => {
         serverResponse = response
@@ -18,10 +19,20 @@ describe('PokemonListService', () => {
   it('Pokémons deve ser um array', () => {
     expect(serverResponse).to.be.an('array')
   })
+  it('Deve retornar 3 Pokémons', () => {
+    expect(serverResponse.length).to.equal(3)
+  })
   it('Objeto Pokémon deve conter um nome e url', () => {
     let pokemon = serverResponse[0]
     expect(pokemon).to.have.property('name')
     expect(pokemon).to.have.property('url')
+  })
+  it('Deve filtrar a lista de Pokémons', done => {
+    service.listPokemons(0, 'bulba')
+      .then(response => {
+        expect(response.length).to.equal(1)
+        done()
+      })
   })
   after(() => {
     removeMock()
